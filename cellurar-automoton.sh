f86f0c5e49317137b434d4ebce3cdc04d0d32cf7
Tobias Thüring <tot@panter.ch>
2017-11-18 17:09:09 +0100

    wip

--- cellurar-automoton.sh
+++ cellurar-automoton.sh
@@ -1,16 +1,35 @@
 #!/usr/bin/env bash
 
 function cell () {
-    state=$1
-    return $state
+	position=$1
+    return $position
 }
 
+function world () {
+	[ "$1" = "register" ] && cell $3
+	pass
+}
 
-# test
+# test 0
 cell 1
 alive=$?
 echo "1 is alive" ${alive}
 
 [ $alive -eq 0 ] || exit 1
 
+
+# test 1
+# 1 alive cell
+# count number of neighbours
+# 0 nb -> ded
+
+# test 2
+# 3 cells
+# 1 alive cell will die
+
+# test world with 1 cell
+cell 1
+world register $?
+
 echo "passed"
+
